eae06e12ec297e534e180f976079471ba4183276
skullikk <skullikk@mail.ru>
2022-12-06 10:23:52 +0300

    Опубликование проекта для сдачи

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os.path
+from typing import Optional
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 from constans import DATA_DIR
 from utils import dict_of_utils
@@ -9,32 +10,30 @@ app = Flask(__name__)
 
 
 @app.route("/perform_query", methods=['POST'])
-def perform_query():
-    file_name = request.args.get('file_name')
-    cmd1 = request.args.get('cmd1')
-    value1 = request.args.get('value1')
-    cmd2 = request.args.get('cmd2')
-    value2 = request.args.get('value2')
+def perform_query() -> Response:
+    file_name: Optional[str] = request.args.get('file_name')
+    cmd1: Optional[str] = request.args.get('cmd1')
+    value1: Optional[str] = request.args.get('value1')
+    cmd2: Optional[str] = request.args.get('cmd2')
+    value2: Optional[str] = request.args.get('value2')
 
-    path = f'{DATA_DIR}\\{file_name}'
+    path: str = f'{DATA_DIR}\\{file_name}'
 
     if not os.path.exists(path):
-        return 'Файл не найден', 400
-    if None in (file_name, cmd1, cmd2, value1, value2):
-        return 'Не все поля заполнены', 400
-    if cmd1 not in dict_of_utils or cmd2 not in dict_of_utils:
-        return 'Неизвестная функция', 400
+        return Response('Файл не найден', status=400)
 
     with open(path) as file:
-        data = file.readlines()
+        data: list[str] = file.readlines()
 
-    first_func = dict_of_utils.get(cmd1)
-    second_func = dict_of_utils.get(cmd2)
+    first_func = dict_of_utils[cmd1]
+    first_res: Optional[list[str]] = first_func(param=value1, data=data)
+    if cmd2 is not None:
+        second_func = dict_of_utils[cmd2]
+        second_res: Optional[list[str]] = second_func(param=value2, data=first_res)
+        return jsonify(second_res)
+    else:
+        return jsonify(first_res)
 
-    first_res = first_func(value1, data)
-    second_res = second_func(value2, first_res)
-
-    return jsonify(list(second_res))
 
 if __name__ == '__main__':
     app.run()
--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
-def filter_query(value, data):
-    return list(filter(lambda x: value in x, data))
+import re
+from typing import Any, Callable
 
 
-def map_query(value, data):
-    col_number = int(value)
+def filter_query(param: str, data: list[str]) -> list[str]:
+    return list(filter(lambda x: param in x, data))
+
+
+def map_query(param: str, data: list[str]) -> list[str]:
+    col_number = int(param)
     return list(map(lambda x: x.split(' ')[col_number], data))
 
 
-def unique_query(value, data):
+def unique_query(data: list[str], *args: Any, **kwargs: Any) -> list[str]:
     result = []
     for str_ in data:
         if str_ not in result:
@@ -15,20 +19,27 @@ def unique_query(value, data):
     return result
 
 
-def sort_query(value, data):
-    reverse = False if value == 'asc' else True
+def sort_query(param: str, data: list[str]) -> list[str]:
+    reverse = False if param == 'asc' else True
     return list(sorted(data, reverse=reverse))
 
 
-def limit_query(value, data):
-    limit = int(value)
+def limit_query(param: str, data: list[str]) -> list[str]:
+    limit = int(param)
     return data[:limit]
 
 
-dict_of_utils = {
+def regex_query(param: str, data: list[str]) -> list[str]:
+    v: str = re.sub(r'\s', '+', param)
+    pattern: re.Pattern = re.compile(v)
+    return list(filter(lambda x: re.search(pattern, x), data))
+
+
+dict_of_utils: dict[str, Callable[..., list[str]]] = {
     'filter': filter_query,
     'map': map_query,
     'unique': unique_query,
     'sort': sort_query,
-    'limit': limit_query
+    'limit': limit_query,
+    'regex': regex_query
 }
